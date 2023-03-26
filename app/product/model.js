596bf2f77c3144e7ce360d6a1ffb6601a8a09a62
LintangVienusa <vienusa6661@gmail.com>
2023-03-26 16:40:03 +0700

    first commit & configuration

---
+++ app/product/model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const { model, Schema } = mongoose;
+
+const productSchema = Schema({
+
+    name: {
+        type: String,
+        minLength: [3, 'Product name must be at least 3 characters'],
+        required: [true, 'Product name is required!']
+    },
+
+    description: {
+        type: String,
+        maxLength: [1000, 'Maximum characters is 1000']
+    },
+
+    price: {
+        type: Number,
+        default: 0
+    },
+
+    img_path: String,
+},{ timestamp: true });
+
+module.exports = model('Product', productSchema);
---
+++ app/product/routes.js
@@ -0,0 +1,8 @@
+const route = require('express').Router();
+const multer = require('multer');
+const os = require('os');
+const productController = require('./controller')
+
+route.post('/products', multer({dest: os.tmpdir()}).single('image'), productController.store)
+
+module.exports = route;
