9a7b0829a6df91be9db8911922aec8e49873088b
LintangVienusa <vienusa6661@gmail.com>
2023-03-28 16:46:47 +0700

    add PUT and DELETE method for products endpoint

--- app/product/controller.js
+++ app/product/controller.js
@@ -59,18 +59,102 @@ const store = async (req, res, next) => {
 
 const index = async (req, res, next) => {
     try {
+        let { skip = 0, limit = 10 } = req.query;
         let product = await Product
         .find()
-        .skip(0)
-        .limit(10);
+        .skip(parseInt(skip))
+        .limit(parseInt(limit));
         return res.json(product);
     } catch (err) {
         next(err);
     }
 }
 
+const update = async (req, res, next) => {
+    try{
+        let { id }= req.params;
+        let payload = req.body;
+
+        if(req.file) {
+            let tmp_path = req.file.path;
+            let originalExt = req.file.originalname.split('.')[req.file.originalname.split('.').length - 1];
+            let filename = req.file.filename + '.' + originalExt;
+            let target_path = path.resolve(config.rootPath, `public/images/products/${filename}`);
+
+            const src = fs.createReadStream(tmp_path);
+            const dest = fs.createWriteStream(target_path);
+            src.pipe(dest);
+
+            src.on('end', async () => {
+                try {
+
+                    let product = await Product.findById(id)
+                    let currImg = `${config.rootPath}/public/images/products/${product.img_path}`
+
+                    if(fs.existsSync(currImg)) {
+                        fs.unlinkSync(currImg);
+                    }
+
+                    product = await Product.findByIdAndUpdate(id, payload, {
+                        new: true,
+                        runValidators: true
+                    });
+                    return res.json(product);
+                } catch(err) {
+                    fs.unlinkSync(target_path);
+                    if(err && err.name == 'ValidationError') {
+                        return res.json({
+                            error: 1,
+                            message: err.message,
+                            fields: err.errors
+                        })
+                    }
+
+                    next(err);
+                }
+            });
+            src.on('error', async () =>{
+                next(err)
+            });
+        }else{
+            let product = await Product.findByIdAndUpdate(id, payload, {
+                new: true,
+                runValidators: true
+            });
+            return res.json(product)
+
+        }
+    } catch(err){
+        if(err && err.name == 'ValidationError') {
+            return res.json({
+                error: 1,
+                message: err.message,
+                fields: err.errors
+            });
+        }
+
+        next(err);
+    }
+}
+
+const destroy = async (req, res, next) => {
+    try {
+        let product = await Product.findByIdAndDelete(req.params.id)
+        let currImg = `${config.rootPath}/public/images/products/${product.img_path}`
+
+        if(fs.existsSync(currImg)) {
+            fs.unlinkSync(currImg);
+        }
+        return res.json(product);
+    } catch(err) {
+        next(err);
+    }
+}
+
 module.exports = {
     store,
-    index
+    index,
+    update,
+    destroy
 
 }
--- app/product/routes.js
+++ app/product/routes.js
@@ -5,5 +5,7 @@ const productController = require('./controller')
 
 route.get('/products', productController.index);
 route.post('/products', multer({dest: os.tmpdir()}).single('image'), productController.store)
+route.put('/products/:id', multer({dest: os.tmpdir()}).single('image'), productController.update)
+route.delete('/products/:id', productController.destroy)
 
 module.exports = route;
