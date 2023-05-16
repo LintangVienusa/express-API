0919b6eb2a211cbe2b85e83f7f0f2f1d5752c898
LintangVienusa <vienusa6661@gmail.com>
2023-05-16 23:42:37 +0700

    add update function in delivery-address endpoint

--- app/deliveryAddress/controller.js
+++ app/deliveryAddress/controller.js
@@ -1,3 +1,5 @@
+const { subject } = require('@casl/ability')
+const { policyFor } = require('../../utils')
 const deliveryAddress = require('./model')
 
 const store = async (req, res, next) => {
@@ -19,6 +21,35 @@ const store = async (req, res, next) => {
         }
 
         next(err); 
+    }
+}
+
+const update = async(req, res, next) => {
+    let policy = policyFor(req.user);
+    try {
+        let { _id, ...payload } = req.body;
+        let { id } = req.params;
+        let address = await deliveryAddress.findById(id);
+        let subjectAddress = subject('deliveryAdress', {...address, user_id: address.user});
+
+        if(!policy.can('update', subjectAddress)) {
+            return res.json({
+                error: 1,
+                message: `You're not allowed to modify this resource`
+            });
+        }
+
+        address = await deliveryAddress.findByIdAndUpdate(id, payload, {new: true});
+        res.json(address);
+    } catch (err) {
+        if(err && err.name == 'ValidationError') {
+            return res.json({
+                error: 1,
+                message: err.message,
+                fields: err.errors
+            });
+        }
+        next(err)
     }
 }
 
